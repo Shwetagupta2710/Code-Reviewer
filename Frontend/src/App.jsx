cd44c7f82d13c88e9a1bc32bca68138324d36887
Shwetagupta2710 <shwetaguptas2710@gmail.com>
2025-04-10 01:34:19 +0530

    add

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,7 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/components/prism-jsx";
+import prism from "prismjs";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,7 +9,25 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  useEffect(() => {
+    prism.highlightAll();
+  });
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <pre>
+              <code className="language-javascript"></code>
+            </pre>
+          </div>
+          <div className="review">Review</div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  );
 }
 
 export default App;
